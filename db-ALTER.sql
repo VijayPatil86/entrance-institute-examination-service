61d07ce03220730fade40567b3c3c10976fafb69
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-30 18:16:24 +0530

    table - QUESTIONS - added column IS_ACTIVE

--- db-ALTER.sql
+++ db-ALTER.sql
@@ -3,3 +3,4 @@ alter table QUESTIONS add column UPDATED_AT timestamp with time zone default cur
 
 alter table QUESTIONS add constraint fk_CORRECT_OPTION_ID foreign key (CORRECT_OPTION_ID) references QUESTION_OPTIONS(OPTION_ID);
 alter table QUESTIONS alter column CORRECT_OPTION_ID set not null;
+alter table QUESTIONS add column IS_ACTIVE boolean not null default true;
