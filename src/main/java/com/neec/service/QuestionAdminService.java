eb22884de16de0634e9f77b04bcc08bfd2df99fa
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-31 19:37:44 +0530

    initial commit

---
+++ src/main/java/com/neec/service/QuestionAdminService.java
@@ -0,0 +1,47 @@
+package com.neec.service;
+
+import com.neec.dto.QuestionRequestDTO;
+import com.neec.dto.QuestionResponseDTO;
+
+public interface QuestionAdminService {
+	/**
+	 * Creates a new question along with its multiple-choice options.
+	 * @param questionDTO The DTO containing the new question and its options.
+	 * @return A DTO representing the newly created question.
+	 */
+	QuestionResponseDTO createQuestion(QuestionRequestDTO questionDTO);
+
+	/**
+     * Retrieves a single question by its ID.
+     * @param questionId The ID of the question to retrieve.
+     * @return A DTO representing the question.
+     */
+    //QuestionResponseDTO getQuestionById(Long questionId);
+
+    /**
+     * Retrieves a paginated list of all questions in the question bank.
+     * @param pageable An object containing pagination information (page number, size).
+     * @return A Page of DTOs representing the questions for the requested page.
+     */
+    //Page<QuestionResponseDTO> getAllQuestions(Pageable pageable);
+
+    /**
+     * Updates an existing question and its options.
+     * @param questionId The ID of the question to update.
+     * @param questionDTO The DTO containing the updated data.
+     * @return A DTO representing the updated question.
+     */
+    //QuestionResponseDTO updateQuestion(Long questionId, QuestionRequestDTO questionDTO);
+
+    /**
+     * Deletes a question from the question bank.
+     * @param questionId The ID of the question to delete.
+     */
+    //void deleteQuestion(Long questionId);
+
+	/**
+     * Restores a question from the question bank.
+     * @param questionId The ID of the question to restore.
+     */
+    //void restoreQuestion(Long questionId);
+}
