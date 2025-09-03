4e55bc80882d4a135838a499c0ae2bd055a3c1c8
VijayPatil86 <vijipatil090386@gmail.com>
2025-09-03 18:04:51 +0530

    added method to get question by id

--- src/main/java/com/neec/repository/QuestionRepository.java
+++ src/main/java/com/neec/repository/QuestionRepository.java
@@ -1,9 +1,14 @@
 package com.neec.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.neec.entity.Question;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-
+	@Query(value = "SELECT q FROM Question q LEFT JOIN FETCH q.correctOption option WHERE q.questionId = :questionId")
+	Optional<Question> findByQuestionIdWithOptions(@Param("questionId") Long questionId);
 }
