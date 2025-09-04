5441e0895aaaf6a00ff9b892db9d947c085702c7
VijayPatil86 <vijipatil090386@gmail.com>
2025-09-04 10:46:33 +0530

    added method to get options for the questions

--- src/main/java/com/neec/repository/QuestionOptionRepository.java
+++ src/main/java/com/neec/repository/QuestionOptionRepository.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.neec.entity.Question;
 import com.neec.entity.QuestionOption;
 
 public interface QuestionOptionRepository extends JpaRepository<QuestionOption, Long> {
 	List<QuestionOption> findByQuestion_QuestionId(Long quetionId);
+	List<QuestionOption> findByQuestionIn(List<Question> questions);
 }
