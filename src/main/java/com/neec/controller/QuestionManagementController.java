5a692667cd8a367a54f38023ecc4edd98cf8f2c1
VijayPatil86 <vijipatil090386@gmail.com>
2025-09-04 20:36:12 +0530

    added request handlers

--- src/main/java/com/neec/controller/QuestionManagementController.java
+++ src/main/java/com/neec/controller/QuestionManagementController.java
@@ -2,12 +2,14 @@ package com.neec.controller;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,22 +63,47 @@ public class QuestionManagementController {
 	 */
 	// http://localhost:9055/api/v1/admin/questions?page=0&size=10&sort=subject,desc&sort=questionDifficultyLevel,asc
 	@GetMapping("/questions")
-	public ResponseEntity<List<QuestionResponseDTO>> getAllQuestions(
+	public ResponseEntity<Page<QuestionResponseDTO>> getAllQuestions(
 		@PageableDefault(page = 0, size = 5, direction = Sort.Direction.DESC, sort = "updatedAt") Pageable pageable ) {
 		return ResponseEntity.ok(questionAdminService.getAllQuestions(pageable));
 	}
 
 	// http://localhost:9055/api/v1/admin/questions?page=0&size=7&sort=questionDifficultyLevel&subject=English
 	@GetMapping(value = "/questions", params = {"subject"})
-	public ResponseEntity<List<QuestionResponseDTO>> getAllQuestionsForSubject(
+	public ResponseEntity<Page<QuestionResponseDTO>> getAllQuestionsForSubject(
 		@RequestParam(name = "subject") String subject,
 		@PageableDefault(page = 0, size = 5, direction = Sort.Direction.DESC, sort = "updatedAt") Pageable pageable ) {
-		return ResponseEntity.ok(questionAdminService.findBySubject(subject, pageable));
+		return ResponseEntity.ok(questionAdminService.getQuestionsBySubject(subject, pageable));
 	}
 
 	@PutMapping("/{questionId}")
 	public ResponseEntity<QuestionResponseDTO> updateQuestion(@PathVariable(name = "questionId") Long questionId,
 			@Valid @RequestBody QuestionRequestDTO questionRequestDTO){
 		return ResponseEntity.ok(questionAdminService.updateQuestion(questionId, questionRequestDTO));
+	}
+
+	@DeleteMapping(value = "/questions/{questionId}")
+	public ResponseEntity<Void> deleteQuestion(@PathVariable(name = "questionId") Long questionId){
+		questionAdminService.deleteQuestion(questionId);
+		return ResponseEntity.noContent().build();
+	}
+
+	@DeleteMapping(value = "/questions/subject/{subject}")
+	public ResponseEntity<Void> deleteQuestionsBySubject(@PathVariable(name = "subject") String subject){
+		questionAdminService.deleteQuestionsBySubject(subject);
+		return ResponseEntity.noContent().build();
+	}
+
+	// why POST for restore - bcoz resource made available to the user, earlier is_active = false
+	@PostMapping("/question/{questionId}/restore")
+	public ResponseEntity<QuestionResponseDTO> restoreQuestion(@PathVariable(name = "questionId") Long questionId){
+		return ResponseEntity.ok(questionAdminService.restoreQuestion(questionId));
+	}
+
+	@PostMapping("/question/subject/{subject}/restore")
+	public ResponseEntity<Page<QuestionResponseDTO>> restoreQuestionsBySubject(
+			@PathVariable(name = "subject") String subject,
+			@PageableDefault(page = 0, size = 5, direction = Sort.Direction.DESC, sort = "updatedAt") Pageable pageable){
+		return ResponseEntity.ok(questionAdminService.restoreQuestionsBySubject(subject, pageable));
 	}
 }
