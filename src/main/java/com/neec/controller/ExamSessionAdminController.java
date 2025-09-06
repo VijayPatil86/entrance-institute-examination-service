5d20a3eb98c1a8b56c69448332b637cd4e41fc47
VijayPatil86 <vijipatil090386@gmail.com>
2025-09-06 17:04:54 +0530

    added request handler method to get all exam sessions

--- src/main/java/com/neec/controller/ExamSessionAdminController.java
+++ src/main/java/com/neec/controller/ExamSessionAdminController.java
@@ -2,15 +2,21 @@ package com.neec.controller;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.neec.dto.ExamSessionDTO;
+import com.neec.enums.ExamStatus;
 import com.neec.service.ExamSessionService;
 
 @RestController
@@ -30,5 +36,13 @@ public class ExamSessionAdminController {
 		return optExamSessionDTO.isPresent() ?
 				ResponseEntity.status(HttpStatus.OK).body(optExamSessionDTO.get()) :
 				ResponseEntity.notFound().build();
+	}
+
+	@GetMapping(path = {"/sessions"})
+	public ResponseEntity<Page<ExamSessionDTO>> getAllExamSessions(
+			@RequestParam(name = "status", required = false) ExamStatus status,
+			@PageableDefault(page = 0, size = 5, direction = Direction.DESC, sort = {"startTime"}) Pageable pageable){
+		// Optional.ofNullable() correctly handles the case where 'status' is null.
+		return ResponseEntity.ok(examSessionService.getAllSessions(Optional.ofNullable(status), pageable));
 	}
 }
